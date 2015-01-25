33949398e9b3f3679989c6478ca7b840cf9e4c8d
ap3rture <ap3rture@users.noreply.github.com>
2015-01-25 11:49:50 -0500

    Update cachematrix.R
    Update of cachematrix.R for Coursera R Programming Course, Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions allow a user to set the value of a matrix, retrieve
+## that matrix, compute the inverse of the matrix, and store and retrieve
+## previously computed inverse values
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function which allows the user to set the initial value
+## of a matrix, retrieve that value, and get the value of the inverse of that 
+## matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        inverse <- NULL
+        get <- function() x  
+        
+        ##retrieve original matrix
+        
+        computeinverse <- function(solve) inverse <<- solve
+        getinverse <- function() inverse
+        
+        ##compute inverse of original matrix 
+        
+        list(get = get, getinverse = getinverse, computeinverse=computeinverse)
+        
+        ##define usable list of functions for makeCacheMatrix
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes, caches and returns the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        inverse <- x$getinverse()
+        
+        ##set the inverse equal to the already computed data, if applicable
+        
+        if(!is.null(inverse)) {
+                message("one moment...retrieving cached data")
+                return(inverse)
+        }
+        
+        ##if inverse has been previously computed, print message stating data
+        ## is being retrieved and return the previously computed data
+        
+        data <- x$get()
+        inverse <- solve(data, ...)
+        inverse
+        
+        ##if inverse has not been previously computed, compute the inverse of
+        ##the original matrix and print the result to the console
 }
